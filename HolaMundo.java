99ec791760b3bf62acdaa36df62bfd15c533a263
Isaac-max-bit <garciaalejoisaac@gmail.com>
2026-01-19 23:44:13 -0500

    new✅

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,6 @@
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola desde Intellij!!!");
+
+        System.out.println("Hola desde Vscode!!!");
     }
 }
